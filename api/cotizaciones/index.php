292976ae149c0c0e06640a13ede90b4673ad13ed
cesar-gimenez <cesar_ximenez@hotmail.com>
2016-11-08 01:43:53 -0300

    App de HTML + JS + REST

---
+++ api/cotizaciones/index.php
@@ -0,0 +1,6 @@
+<?php 
+	header("Content-Type:application/json");
+
+	header('Access-Control-Allow-Origin: *'); 
+	header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+	echo file_get_contents('data.json');
